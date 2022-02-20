40b4d43117d6940390d3842d77d8a0a74491bad5
Salomao S <souza95salomao@gmail.com>
2022-02-20 16:28:58 -0400

    [FIX] Deployment PORT config

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,5 @@
 import { /*serverHTTP,*/ app } from './server'
-const PORT = 9000
+const PORT = process.env.PORT || 9000;
 const MESSAGE = `server is running on port ${PORT}`
 
 app.listen(PORT,	()=>console.log(MESSAGE));
